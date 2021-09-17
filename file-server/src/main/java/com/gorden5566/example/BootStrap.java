4a876a578872f8772944af83c759a5ec462f15bd
gorden5566 <gorden5566@126.com>
2021-09-18 00:43:22 +0800

    Compile to a single executable JAR file

---
+++ file-server/src/main/java/com/gorden5566/example/BootStrap.java
@@ -0,0 +1,13 @@
+package com.gorden5566.example;
+
+import com.gorden5566.example.http.HttpStaticFileServer;
+
+/**
+ * @author gorden5566
+ * @date 2021/09/18
+ */
+public class BootStrap {
+    public static void main(String[] args) throws Exception {
+        HttpStaticFileServer.main(args);
+    }
+}
