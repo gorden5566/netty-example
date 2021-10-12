ad33ea895bb273c251200f77b9bcb75a53ec1da4
gorden5566 <gorden5566@126.com>
2021-10-12 23:18:29 +0800

    Add echo server

---
+++ echo-server/src/main/java/com/gorden5566/example/BootStrap.java
@@ -0,0 +1,11 @@
+package com.gorden5566.example;
+
+/**
+ * @author gorden5566
+ * @date 2021/09/23
+ */
+public class BootStrap {
+    public static void main(String[] args) throws Exception {
+        Command.run(args);
+    }
+}
---
+++ echo-server/src/main/java/com/gorden5566/example/EchoServerHandler.java
@@ -0,0 +1,29 @@
+package com.gorden5566.example;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handler implementation for the echo server.
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
